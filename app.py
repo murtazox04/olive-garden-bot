c540b8069674f0400b7ff022b52e2caabdd465f2
murtazox04 <murtazox04@gmail.com>
2024-05-11 01:14:32 +0500

    First commit & Basic settings are set

---
+++ app.py
@@ -0,0 +1,27 @@
+from aiogram import Dispatcher,  types
+from loader import app, bot, dp, WEBHOOK_PATH, WEBHOOK_URL
+
+
+@app.on_event("startup")
+async def on_startup() -> None:
+    url = await bot.get_webhook_info()
+
+    if url != WEBHOOK_URL:
+        await bot.set_webhook(
+            url=WEBHOOK_URL
+        )
+
+
+@app.post(WEBHOOK_PATH)
+async def bot_webhook(update: dict) -> None:
+    telegram_update = types.Update(**update)
+    await Dispatcher._feed_webhook_update(
+        self=dp,
+        bot=bot,
+        update=telegram_update
+    )
+
+
+@app.on_event("shutdown")
+async def on_shutdown() -> None:
+    await dp.storage.close()
